e0cdd2734af168031a2b62230db766529e65ceda
maulanafadilah <maulanafadilah@student.telkomuniversity.ac.id>
2022-06-13 12:13:22 +0700

    pusat-bantuan

--- resources/views/sellers/dashboard/pusatbantuan.blade.php
+++ resources/views/sellers/dashboard/pusatbantuan.blade.php
@@ -9,8 +9,8 @@
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Hubungi Kami</h5>
-                    <p class="card-text">E-Mail : emailsiapaajaini@gmail.com <br>
-                    No Telepon : 0821*******</p>
+                    <p class="card-text">E-Mail : babydailyindonesia@gmail.com <br>
+                    No Telepon : 082215288085</p>
                 </div>
             </div>
         </div>
